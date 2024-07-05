07529356c5a9dc13f494d70fe2b4cf981ea8e8ed
Harsh <harshsantoshi05@gmail.com>
2024-07-05 12:51:24 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import QuoteCard from './components/QuoteCard';
+import SavedQuotes from './components/SavedQuotes';
+import './App.css';
+
+function App() {
+  const [quote, setQuote] = useState('');
+  const [savedQuotes, setSavedQuotes] = useState([]);
+
+  const fetchQuote = async () => {
+    const response = await axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes');
+    setQuote(response.data[0]);
+  };
+
+  const saveQuote = (quote) => {
+    setSavedQuotes([...savedQuotes, quote]);
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Ron Swanson Quotes</h1>
+      <QuoteCard quote={quote} onSave={saveQuote} />
+      <button className="fetch-button" onClick={fetchQuote}>Get New Quote</button>
+      <SavedQuotes savedQuotes={savedQuotes} />
+    </div>
+  );
+}
+
+export default App;
